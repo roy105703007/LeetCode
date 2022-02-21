00d451c2c2204dae0e2b588093f4d8528a7ea5bb
roy105703007 <roy990427@gmail.com>
2022-02-21 20:41:08 +0800

    121

---
+++ 121. Best Time to Buy and Sell Stock/121. Best Time to Buy and Sell Stock.go
@@ -0,0 +1,13 @@
+func maxProfit(prices []int) int {
+	min := prices[0]
+	max := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i]-min > max {
+			max = prices[i] - min
+		}
+		if prices[i] < min {
+			min = prices[i]
+		}
+	}
+	return max
+}
