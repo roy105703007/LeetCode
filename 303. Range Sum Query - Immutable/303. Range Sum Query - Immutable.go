fef0354005b46b4c539bca0c10afcd34fc519904
roy105703007 <roy990427@gmail.com>
2022-03-16 00:53:19 +0800

    303, 64

---
+++ 303. Range Sum Query - Immutable/303. Range Sum Query - Immutable.go
@@ -0,0 +1,23 @@
+type NumArray struct {
+	preSum []int
+}
+
+func Constructor(nums []int) NumArray {
+	for i := 1; i < len(nums); i++ {
+		nums[i] += nums[i-1]
+	}
+	return NumArray{preSum: nums}
+}
+
+func (this *NumArray) SumRange(left int, right int) int {
+	if left > 0 {
+		return this.preSum[right] - this.preSum[left-1]
+	}
+	return this.preSum[right]
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * obj := Constructor(nums);
+ * param_1 := obj.SumRange(left,right);
+ */
---
+++ 64. Minimum Path Sum/64. Minimum Path Sum.go
@@ -0,0 +1,24 @@
+func minPathSum(grid [][]int) int {
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if i == 0 {
+				grid[i][j] = grid[i][j-1] + grid[i][j]
+			} else if j == 0 {
+				grid[i][j] = grid[i-1][j] + grid[i][j]
+			} else {
+				grid[i][j] = min(grid[i-1][j], grid[i][j-1]) + grid[i][j]
+			}
+		}
+	}
+	return grid[len(grid)-1][len(grid[0])-1]
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
