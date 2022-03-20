70469971b9faa18bb980b4640fb164ec635e7c90
roy105703007 <roy990427@gmail.com>
2022-03-20 23:01:37 +0800

    322

---
+++ 322. Coin Change/322. Coin Change.go
@@ -0,0 +1,26 @@
+func coinChange(coins []int, amount int) int {
+	dp := make([]int, amount+1)
+	for i := range dp {
+		dp[i] = amount + 1
+	}
+	dp[0] = 0
+	//dp[i] = min(dp[i], dp[i-coin] + 1)
+	for i := 1; i < amount+1; i++ {
+		for j := 0; j < len(coins); j++ {
+			if coins[j] <= i {
+				dp[i] = min(dp[i], dp[i-coins[j]]+1)
+			}
+		}
+	}
+	if dp[amount] > amount {
+		return -1
+	}
+	return dp[amount]
+}
+
+func min(i, j int) int {
+	if i < j {
+		return i
+	}
+	return j
+}
